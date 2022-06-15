bf04cd0d1ffd24bddb002c9b6cd37a0f710ff4da
ntv2002a <trungvu16502@gmail.com>
2022-06-15 12:21:25 +0700

    Commit at 15/6/2022
    Quen commit len vi hieu sai cach hoat dong cua github

---
+++ PRJ_301_Test1/src/java/object/Circles.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package object;
+
+/**
+ *
+ * @author trung
+ */
+public class Circles {
+    private int id;
+    private int x;
+    private int y;
+    private int radius;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+}
